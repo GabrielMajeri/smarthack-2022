c4bd013e4a4a33443245eb90ca00bc39c1a7edb7
DimaOanaTeodora <oanadima26@gmail.com>
2022-11-05 15:29:26 +0200

    Added template

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,13 @@
 import "../styles/globals.css";
+import "../styles/SectionOne.scss";
+import "../styles/About.scss";
+import "../styles/Content.scss";
+import "../styles/Footer.scss";
+import "../styles/Header.scss";
+import "../styles/SectionFive.scss";
+import "../styles/SectionFour.scss";
+import "../styles/SectionThree.scss";
+import "../styles/SectionTwo.scss";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
