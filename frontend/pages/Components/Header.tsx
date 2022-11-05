d9ed6209723ad180ee44db9de111ed08e34a2a4e
Oana Dima <61749814+DimaOanaTeodora@users.noreply.github.com>
2022-11-05 20:02:06 +0200

    1st page final cut (#4)

--- frontend/pages/Components/Header.tsx
+++ frontend/pages/Components/Header.tsx
@@ -1,66 +1,115 @@
-import { Button, Badge, Burger, Drawer, Code, Title, Anchor, Text } from '@mantine/core';
-import { UnstyledButton, Group, Avatar } from '@mantine/core';
-import React from 'react';
-import { Link } from 'react-scroll';
+import {
+  Button,
+  Badge,
+  Burger,
+  Drawer,
+  Code,
+  Title,
+  Anchor,
+  Text,
+  useMantineTheme,
+} from "@mantine/core";
+import { UnstyledButton, Group, Avatar } from "@mantine/core";
+import React from "react";
+import { Link } from "react-scroll";
 
 const Header = () => {
-    //const theme = useMantineTheme();
-    const [opened, setOpened] = React.useState(false);
-    const title = opened ? 'Close navigation' : 'Open navigation';
+  const theme = useMantineTheme();
+  const [opened, setOpened] = React.useState(false);
+  const title = opened ? "Close navigation" : "Open navigation";
 
-    return (
-        <header>
-            <div className="content-desktop">
-                <div>
-                    <Badge size="lg" radius={10} color="yellow">A simple Mantine template</Badge>
-                </div>
-                <div className="navbar">
-                    <div className="navbar-item"><Link to="section-one" smooth duration={500}>Carousel</Link></div>
-                    <div className="navbar-item"><Link to="section-four" smooth duration={500}>Cards</Link></div>
-                    <div className="navbar-item"><Link to="section-five" smooth duration={500}>FAQS</Link></div>
+  return (
+    <header>
+      <div className="content-desktop">
+        <div>
+          {/* <Badge size="lg" radius={10} color="yellow">A simple Mantine template</Badge> */}
+        </div>
+        <div className="navbar">
+          <div className="navbar-item">
+            <Link to="section-four" smooth duration={500}>
+              Ce facem?
+            </Link>
+          </div>
+          <div className="navbar-item">
+            <Link to="section-five" smooth duration={500}>
+              Întrebări frecvente
+            </Link>
+          </div>
 
-                    <Button color="yellow" onClick={() => redirectToLink('https://mantine.dev/')}>Check out Mantine</Button>
-                </div>
+          <Button
+            style={{ background: "var(--custom-red)" }}
+            onClick={() => redirectToLink("https://mantine.dev/")}
+          >
+            Conectare
+          </Button>
+        </div>
+      </div>
+
+      <div className="content-mobile">
+        <div className="burger-button">
+          <Burger
+            opened={opened}
+            onClick={() => setOpened((o) => !o)}
+            title={title}
+            size="sm"
+          />
+        </div>
+
+        <Drawer
+          transition="rotate-right"
+          transitionDuration={250}
+          transitionTimingFunction="ease"
+          overlayOpacity={0.55}
+          position="right"
+          closeButtonLabel="Close drawer"
+          title="Menu"
+          padding="xl"
+          opened={opened}
+          onClose={() => setOpened(false)}
+        >
+          <div className="menu">
+            <div className="menu-items">
+              <div className="menu-item">
+                <Link
+                  to="section-one"
+                  smooth
+                  duration={500}
+                  onClick={() => setOpened(false)}
+                >
+                  <Title order={2}>Carousel</Title>
+                </Link>
+              </div>
+              <div className="menu-item">
+                <Link
+                  to="section-four"
+                  smooth
+                  duration={500}
+                  onClick={() => setOpened(false)}
+                >
+                  <Title order={2}>Cards</Title>
+                </Link>
+              </div>
+              <div className="menu-item">
+                <Link
+                  to="section-five"
+                  smooth
+                  duration={500}
+                  onClick={() => setOpened(false)}
+                >
+                  <Title order={2}>FAQs</Title>
+                </Link>
+              </div>
             </div>
 
-            <div className="content-mobile">
-                <div className="burger-button">
-                    <Burger
-                        opened={opened}
-                        onClick={() => setOpened((o) => !o)}
-                        title={title}
-                        size="sm"
-                    />
-                </div>
+            <div className="menu-items">
+              <Text>Contact</Text>
 
-                <Drawer
-                    transition="rotate-right"
-                    transitionDuration={250}
-                    transitionTimingFunction="ease"
-                    overlayOpacity={0.55}
-                    position="right"
-                    closeButtonLabel="Close drawer"
-                    title="Menu"
-                    padding="xl"
-                    opened={opened}
-                    onClose={() => setOpened(false)}
-                >
-                    <div className="menu">
-                        <div className="menu-items">
-                            <div className="menu-item"><Link to="section-one" smooth duration={500} onClick={() => setOpened(false)}><Title order={2}>Carousel</Title></Link></div>
-                            <div className="menu-item"><Link to="section-four" smooth duration={500} onClick={() => setOpened(false)}><Title order={2}>Cards</Title></Link></div>
-                            <div className="menu-item"><Link to="section-five" smooth duration={500} onClick={() => setOpened(false)}><Title order={2}>FAQs</Title></Link></div>
-                        </div>
+              <Anchor href="mailto: contact@autoflow.ro">
+                contact@autoflow.ro
+              </Anchor>
+            </div>
 
-                        <div className="menu-items">
-                            <Text>Contact</Text>
-
-                            <Anchor href="mailto:example@mail.com">
-                                example@mail.com
-                            </Anchor>
-                        </div>
-
-                        <Code color="yellow" style={{ display: 'flex', flexDirection: 'column', gap: 6, padding: 15 }}>
+            {/* <Code style={{ background: "var(--custom-red)", display: 'flex', flexDirection: 'column', gap: 6, padding: 15 }}>
                             Template made by
                             <Anchor href="https://github.com/Giuseppetm">
                                 <UnstyledButton>
@@ -73,16 +122,16 @@ const Header = () => {
                                     </Group>
                                 </UnstyledButton>
                             </Anchor>
-                        </Code>
-                    </div>
-                </Drawer>
-            </div>
-        </header>
-    );
+                        </Code> */}
+          </div>
+        </Drawer>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
 
 const redirectToLink = (link: string): void => {
-    window.open(link, '_blank');
+  window.open(link, "_blank");
 };
