877f35e7787a123d47db2aa38660f895acc5ff4f
Andrei Constantinescu <andreiec112@gmail.com>
2022-11-06 03:37:27 +0200

    Added forms microservice

---
+++ backend/forms/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+from MongoAPI import MongoAPI
+
+app = Flask(__name__)
+
+mongo_api = MongoAPI({'database': 'smarthack', 'collection': 'forms'})
+
+
+@app.route('/forms/submit', methods=['POST'])
+def submit_form():
+    if request.method == 'POST':
+        result = mongo_api.collection.insert_one(request.json).inserted_id
+        return jsonify({'id': str(result)})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5003)
